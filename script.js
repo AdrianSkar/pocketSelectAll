903968279d30227a8937365e5dacec33f0cd7125
Adrian Skar <adrian@adrianskar.com>
2020-05-17 16:00:57 +0200

    First version
    Needs to be copied and pasted into the console

---
+++ script.js
@@ -0,0 +1,24 @@
+// https://stackoverflow.com/questions/2705583/how-to-simulate-a-click-with-javascript
+function eventFire(el, etype){
+  if (el.fireEvent) {
+    el.fireEvent('on' + etype);
+  } else {
+    var evObj = document.createEvent('Events');
+    evObj.initEvent(etype, true, false);
+    el.dispatchEvent(evObj);
+  }
+}
+
+//svg
+var target = document.getElementsByTagName("use");
+
+//loop trough <use> elements (some are not articles)
+for (let i = 0; i < target.length; i++) {
+	// check if target are selectable articles
+	if ((target[i].getAttribute('xlink:href').indexOf("CheckOpen") > -1)) {
+		//click on them
+		eventFire(target[i], 'click');
+	}
+	else { console.log(target[i], 'is not clickable')}
+	// eventFire(cells, 'click');
+}
