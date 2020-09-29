7aa378106ab3261ed21f3a6b54c3f6d8410e27f5
AdrianSkar <adrian@adrianskar.com>
2020-09-29 16:29:00 +0200

    refactor[scripts|docs]: Minor syntax and doc improv.

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # pocketSelectAll
-Add the ability to select all Pocket articles
+Add the ability to select all [Pocket](https://app.getpocket.com/) articles. This process can be easily automated using tools like Integromat but I'll test custom JS scripts for practicing purposes.
 
 Inspired in part by [this script](https://greasyfork.org/en/scripts/39554-pocket-select-all).
+
 Will try to implement a button to use it in the same way but for now you need to copy the script.js code and paste it into the console after enabling bulk edit and it will select all visible articles. 
+
 ![console behaviour gif](console_b.gif)
--- script.js
+++ script.js
@@ -1,18 +1,16 @@
-// This process can be easily automated using tools like integromat. Testing scripts for practicing purposes
-
 // https://stackoverflow.com/questions/2705583/how-to-simulate-a-click-with-javascript
-function eventFire(el, etype){
-  if (el.fireEvent) {
-    el.fireEvent('on' + etype);
-  } else {
-    var evObj = document.createEvent('Events');
-    evObj.initEvent(etype, true, false);
-    el.dispatchEvent(evObj);
-  }
+function eventFire(el, etype) {
+	if (el.fireEvent) {
+		el.fireEvent('on' + etype);
+	} else {
+		let evObj = document.createEvent('Events');
+		evObj.initEvent(etype, true, false);
+		el.dispatchEvent(evObj);
+	}
 }
 
 //svg
-var target = document.getElementsByTagName("use");
+let target = document.getElementsByTagName("use");
 
 //loop trough <use> elements (some are not articles)
 for (let i = 0; i < target.length; i++) {
@@ -21,6 +19,6 @@ for (let i = 0; i < target.length; i++) {
 		//click on them
 		eventFire(target[i], 'click');
 	}
-	else { console.log(target[i], 'is not clickable')}
+	else { console.log(target[i], 'is not clickable'); }
 	// eventFire(cells, 'click');
 }
