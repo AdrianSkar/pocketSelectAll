ade340319f8a123b28e590f092b076fadb47f451
AdrianSkar <adrian@adrianskar.com>
2020-10-06 16:34:01 +0200

    feat: add `selectAll` button + update readme
    Add the ability to select all articles within the current page without
    having to paste/execute the script manually until done.
    This closes #3 and closes #4

--- README.md
+++ README.md
@@ -3,14 +3,18 @@ Add the ability to select all [Pocket](https://app.getpocket.com/) articles. Thi
 
 I'll implement easier ways to use this but for now you'd need to follow this simple steps:
 
-1. Enable the bulk edit option within Pocket.
+1. Enable the bulk edit option within Pocket.<br>
 		![bulk edit button image](images/bulk_edit.png)
 2. Copy the `script.js` code and paste it into your browser's console (F12 or Menu -> Dev tools) and press `Enter`.
    
-This will show a button allowing you to select all visible articles (Pocket currently only shows around 25 at a time). If you want to select more you can scroll down and click the button again.
+This will show a couple of buttons allowing you to select all visible articles (Pocket currently only shows around 25 at a time) or all articles within the page. 
 
-![Select visible button screenshot](images/selectVisible.png)
+![Select visible button screenshot](images/buttons.png)
 
+When using the *Select all* option the script will select all visible articles, scroll down and wait 2 seconds for the new batch to load (showing *working...* within the button) until it finds no more articles to select (showing *done* within the button).
+
+### Notes:
+- The *Select all* script has been tested to work properly down to 1mb speeds, if you have a slower connection try raising the interval time at line 63 (`timer` function) 
 ### - Refs:
 
 - Inspired in part by [this Greasy Fork script](https://greasyfork.org/en/scripts/39554-pocket-select-all).
--- script.js
+++ script.js
@@ -9,30 +9,60 @@ let selectVis = function () {
 			el.dispatchEvent(evObj);
 		}
 	}
-
 	//svg
 	let target = document.getElementsByTagName("use");
 
 	//loop trough <use> elements (some are not articles)
 	for (let i = 0; i < target.length; i++) {
-		// check if target are selectable articles
+		// check if targets are selectable articles
 		if ((target[i].getAttribute('xlink:href').indexOf("CheckOpen") > -1)) {
 			//click on them
 			eventFire(target[i], 'click');
 		}
 		else { console.log(target[i], 'is not clickable'); }
 	}
+
 };
 
-//prepend a button before the close bulk edit one
-
-let bulk = document.querySelector("button[aria-label='Close Bulk Edit']");
-let selVis = bulk.cloneNode();
+//prepend buttons before the close bulk edit one
+let bulk = document.querySelector("button[aria-label='Close Bulk Edit']"),
+	selVis = bulk.cloneNode(),
+	selAll = bulk.cloneNode();
 
 selVis.textContent = 'Select visible';
-selVis.setAttribute('margin-right', '1em');
+selAll.textContent = 'Select all';
+selVis.setAttribute('margin-right', '1em'); // todo: fix; not working
+selAll.setAttribute('margin-right', '1em');
 
 bulk.parentElement.prepend(selVis);
+bulk.parentElement.prepend(selAll);
+
+// select all articles (selectVis + scroll every 2secs)
+let selectAll = function () {
+	let target = document.getElementsByTagName("use"),
+		last = target[target.length - 1];
+
+	function scrollEnd() {
+		if (last.getAttribute('xlink:href').indexOf("CheckOpen") === -1) {
+			selAll.textContent = 'Done';
+			console.log('done');
+			clearInterval(timer);
+			selectVis();
+		}
+		else {
+			selectVis();
+			last.scrollIntoView();
+			selAll.textContent = 'working...';
+			console.log('scrolling');
+		}
+	}
+	let timer = setInterval(() => {
+		target = document.getElementsByTagName("use");
+		last = target[target.length - 1];
+		scrollEnd();
+	}, 2000);
+};
 
 selVis.addEventListener('click', selectVis);
+selAll.addEventListener('click', selectAll);
 
